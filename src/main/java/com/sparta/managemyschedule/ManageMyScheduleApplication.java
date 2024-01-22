cbe7194038049e685e7336241a1f258114003034
9noeyni9 <koo949494@naver.com>
2024-01-22 12:57:54 +0900

    first commit

---
+++ src/main/java/com/sparta/managemyschedule/ManageMyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.managemyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMyScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageMyScheduleApplication.class, args);
+    }
+
+}
