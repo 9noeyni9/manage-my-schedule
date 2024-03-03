c44f92a198d8acc2838a457854c48e5c26159718
9noeyni9 <koo949494@naver.com>
2024-03-03 09:41:23 +0900

    feat : 댓글 부분 entity 추가

---
+++ src/main/java/com/sparta/managemyschedule/entity/Comment.java
@@ -0,0 +1,37 @@
+package com.sparta.managemyschedule.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment extends ScheduleDate{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String Comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="schedule_id", nullable = false)
+    private Schedule schedule;
+
+}
